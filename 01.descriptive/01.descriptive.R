76ecfe020129b44de114d4db057beb63ceef1b89
joao-med <72229642+joao-med@users.noreply.github.com>
2023-04-27 11:42:23 -0300

    Add files via upload

--- 01.descriptive/01.descriptive.R
+++ 01.descriptive/01.descriptive.R
@@ -4,7 +4,6 @@
 options(scipen=999)
 # requiring packages
 library(tidyverse)
-library(raster)
 library(geofacet)
 require(tmap)
 library(geobr)
@@ -13,9 +12,8 @@ library(RColorBrewer)
 library(xlsx)
 library(patchwork)
 library(gridExtra)
+library(ggsn)
 theme_set(theme_bw())
-# setwd('final_codes/final_final_codes')
-
 # loading data
 data <- read_rds("00.data/data.rds")
 grid <- read.csv("mg_grid.csv") %>% dplyr::select(-X)
@@ -25,74 +23,11 @@ filter <- data %>% group_by(code) %>%
   summarise(sum = sum(cases),code_meso) %>%
   filter (sum >= 10000) %>% arrange(code_meso) %>% unique
 # Map minas ---------------------------------------------------------------
-############### GRAFICO SEM MESO
-# Minas gerais geometry
-# MG <- read_micro_region() %>% filter(abbrev_state %in% "MG")
-# MG <- read_meso_region() %>% filter(abbrev_state %in% "MG")
-# MG %>% plot
-# MG <- MG %>% mutate(cat = ifelse(code_micro %in% filter$code, "Included", "Excluded"),
-#                     id = 1:66)
-# p1 <- MG %>%
-#   ggplot()+
-#   geom_sf(aes(fill = cat))+
-#   geom_sf_text(aes(label = id),color = 'white')+
-#   scale_fill_manual(values = c("grey", "black"), name = NULL)+
-#   scale_color_discrete(breaks = gtools::mixedsort(MG$name_state))+
-#   theme(legend.text = element_text(margin = margin(0, 0, 0, -24)))+
-#   xlab("")+
-#   ylab("")+
-#   theme_minimal()
-# MG %>%
-#   ggplot()+
-#   geom_sf(aes(fill = ))+
-#   geom_sf_text(aes(label = id),color = 'white')+
-#   scale_fill_manual(values = c("grey", "black"), name = NULL)+
-#   scale_color_discrete(breaks = gtools::mixedsort(MG$name_state))+
-#   theme(legend.text = element_text(margin = margin(0, 0, 0, -24)))+
-#   xlab("")+
-#   ylab("")+
-#   theme_minimal()
-# p1+   tableGrob (cbind(MG$id[1:22],
-#                  MG$name_micro[1:22],
-#                  MG$id[23:44],
-#                  MG$name_micro[23:44],
-#                  MG$id[45:66],
-#                  MG$name_micro[45:66]))
-
-#####################
-
-# ggsave("03.figs/fig01.png", height = 7, width = 16)
 MG <- read_micro_region() %>% filter(abbrev_state %in% "MG")
 MG_meso <- read_meso_region() %>% filter(abbrev_state %in% "MG")
 MG <- MG %>% mutate(cat = ifelse(code_micro %in% filter$code, "Included", "Excluded")) %>%
   left_join(data[,c(1,28,29)] %>% unique, by  = c('code_micro'= 'code')) %>% arrange(code_meso) %>% 
   mutate(id = 1:66)
-
-p1 <-    
-  ggplot(MG)+
-  geom_sf(data=MG, aes(fill = cat))+
-  geom_sf(data=MG_meso, linetype =1, color = "black", fill= NA, alpha = 1)+
-  geom_sf_text(aes(label = id),color = 'black')+ 
-  scale_fill_manual(values = c("#D3D3D3", "#899499"), name = NULL)+
-  
-  scale_color_discrete(breaks = gtools::mixedsort(MG$name_state))+
-  theme(legend.text = element_text(margin = margin(0, 0, 0, -24)))+
-  xlab("")+
-  ylab("")+
-  theme_minimal()
-
-table_MG <- MG[,c(5,8,10)] %>% sf::st_drop_geometry() %>% as.data.frame()
-colnames(table_MG) <- c("Microrregião", "Mesorregião","ID")
-rownames(table_MG) <- c(NULL)
-p1 + tableGrob (cbind(table_MG[1:22,],
-                       table_MG[23:44,],
-                       table_MG[45:66,]),
-                 rows = c("","","","","","","","","","",
-                          "","","","","","","","","","",
-                          "",""))
-ggsave("03.figs/fig01.png", height = 7, width = 25)
-
-
 # Dengue ------------------------------------------------------------------
 # sum of cases
 summary(data)
@@ -100,35 +35,39 @@ data$cases %>% sum
 # daily distribution by microregion
 tab_cases <- data %>%
   group_by(microregion_name) %>% 
-  summarize(Min=min(cases),
-            Q1=quantile(cases,0.25),
-            Q2=quantile(cases,0.5),
-            Q3=quantile(cases,0.75),
-            Max=round(max(cases),2),
-            Mean=round(mean(cases),2),
-            DP=round(sd(cases),2),
-            Sum_cases = sum(cases),
-            Mean_pop=round(mean(pop),0),
-            Rate = round(Sum_cases/Mean_pop*10^5,2),
-            annual_cases_mean = round(Sum_cases/10))
+  summarize(
+    Mean_pop=round(mean(pop),0),
+    Sum_cases = sum(cases),
+    annual_cases_mean = round(Sum_cases/10),
+    Rate_annual = round(annual_cases_mean/Mean_pop*10^5,2),
+    Min=min(cases),
+    Percentil_25=quantile(cases,0.25),
+    Mediana=quantile(cases,0.5),
+    Mean=round(mean(cases),2),
+    Percentil_75=quantile(cases,0.75),
+    Max=round(max(cases),2),
+    DP=round(sd(cases),2))
 #Saving table
 write.xlsx(tab_cases, "01.descriptive/tab_cases.xlsx")
-
 # Exploring all cases -----------------------------------------------------
 # time series
 p1 <- data %>% 
   ggplot(aes(x = date, y = cases))+
   geom_line()+
+  xlab('Data')+
+  ylab("Casos")
   ggtitle('A')
 # by month
 p2 <- ggplot(data=data,aes(x=as.factor(month), y=cases)) +
   geom_bar(stat = "identity")+
-  xlab ("month")+
+  xlab ("Mês")+
+  ylab('Casos')+
   ggtitle('B')
 # by day of the week
 p3 <- ggplot(data=data,aes(x=as.factor(weekday), y=cases)) +
-  geom_bar(stat = "identity") +
-  xlab ("weekday") +
+  geom_bar(stat = "identity", fill = "black") +
+  xlab ("Dia da Semana") +
+  ylab("Casos")+
   theme(axis.text.x=element_text(angle=45, hjust=1))+
   ggtitle('C')
 #Sunday and Saturday are the days with the least cases
@@ -142,30 +81,35 @@ p4 <- data %>% mutate(year = year %>% as.factor) %>%
   mutate (rate = cases/pop*10^5) %>%
   ggplot(aes(month, year, fill= rate)) +
   geom_raster()+
-  ylab("Year") + 
-  xlab("Month") +
-  scale_fill_gradientn(name = "Rate", colours=brewer.pal(9, "Greys"), trans = "log1p",
+  ylab("Ano") + 
+  xlab("Mês") +
+  scale_fill_gradientn(name = "Taxa", colours=brewer.pal(9, "Greys"), trans = "log1p",
                        breaks = c(0, 10, 100, 300, 1000), labels = c(0, 10, 100, 300, 1000)) +
   scale_x_discrete(breaks = c(1,4,7,10), labels = c("Jan", "Apr", "Jul", "Oct")) +
   ggtitle('D')
-
-(p1 | p2 )/ (p3 | p4)
-ggsave("03.figs/fig02.png", dpi = 200, width = 8, height = 8)
-# heat map based on year, month and rate by microregion  ---------------------------------------------
-data %>% mutate(year = year %>% as.factor) %>% 
+# heat map based on year, month and rate by microregion  -------------------------
+p5 <- data %>% mutate(year = year %>% as.factor) %>% 
   group_by(year, month, name, pop, code) %>% 
   summarise(cases = sum (cases),
             pop = max(pop)) %>% 
   mutate (rate= cases/pop*10^5)  %>%
   ggplot(aes(month, year, fill= rate)) +
   geom_raster()+
-  ylab("Year") + 
-  xlab("Month") +
-  scale_fill_gradientn(name = "Rate", colours=brewer.pal(9, "Greys"), trans = "log1p",
+  ylab("Ano") + 
+  xlab("Mês") +
+  scale_fill_gradientn(name = "Taxa", colours=brewer.pal(9, "Greys"), trans = "log1p",
                        breaks = c(0, 10, 100, 300, 1000), labels = c(0, 10, 100, 300, 1000)) +
   scale_x_discrete(breaks = c(1,4,7,10), labels = c("Jan", "Apr", "Jul", "Oct")) +
-  facet_geo(~name, grid = grid)
-ggsave("03.figs/fig03.png", dpi = 200, width = 12, height = 13)
+  facet_geo(~name, grid = grid)+
+  ggtitle('E')
+# Layout for plot
+layout <- "
+AABBEEEE
+CCDDEEEE
+"
+p1 + p2 + p3 + p4 + p5 +
+  plot_layout(design = layout)
+ggsave("03.figs/fig01.png", dpi = 600, width = 24, height = 15)
 
 # Temperature -------------------------------------------------------------
 
@@ -223,13 +167,7 @@ for (i in data$microregion_name  %>% unique) {
   big_table <- bind_rows(lil_table, big_table) %>% arrange(i)
 }
 write.xlsx(big_table, "01.descriptive/table_temp.xlsx")
-####  maps of average min temperature by microregion
-# Yearly min temp
-p1 <- yearly_ERA_mg %>% ggplot(aes(fill = t_min)) +
-  geom_sf ()+
-  scale_fill_gradientn(name = "TºC min", colours=brewer.pal(9, "Greys"),
-                       breaks = c(14, 16, 18, 20), labels = c('14ºC', '16ºC', '18ºC', '20ºC')) +
-  ggtitle("A")
+
 # create a list with regional datasets
 regions <- data$microregion_name %>% unique()
 data_region <- lapply(regions,function(x) data[data$microregion_name==x,])
@@ -242,17 +180,3 @@ ranges_data$indice <- 1:length(regions)
 ranges_data$region <- row.names(ranges_data)
 ranges_data$temperature <- (ranges_data$X1+ranges_data$X2)/2 
 ranges_data <-  ranges_data %>% as.tibble()
-
-p2 <- ranges_data %>% 
-  ggplot(aes(x = reorder(region,temperature), y = temperature))+
-  geom_point()+
-  geom_errorbar(aes(ymin= X1, ymax = X2),
-                width = 0.8)+
-  ylab ("Temperature (ºC)")+
-  theme(axis.title.y = element_blank())+
-  coord_flip()+
-  ggtitle("B")
-
-p1/p2
-ggsave('03.figs/fig04.png', height = 11)
-
